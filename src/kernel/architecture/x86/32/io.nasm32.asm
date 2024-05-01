59aff2fa9ea28f189c57a04c61a0e60a55116b29
leg7 <dev@leonardgomez.xyz>
2024-05-01 12:00:20 +0000

    architecture/x86/32: fix 32bit IO functions

--- src/kernel/architecture/x86/32/io.nasm32.asm
+++ src/kernel/architecture/x86/32/io.nasm32.asm
@@ -4,35 +4,35 @@ global x86_32_out_8, x86_32_out_16, x86_32_out_32, x86_32_in_8, x86_32_in_16, x8
 section .text
 
 x86_32_out_8:
-	mov edx, [ebp + 8]
-	mov eax, [ebp + 12]
+	mov edx, [esp + 4]
+	mov eax, [esp + 8]
 	out dx, al
 	ret
 
 x86_32_out_16:
-	mov edx, [ebp + 8]
-	mov eax, [ebp + 12]
+	mov edx, [esp + 4]
+	mov eax, [esp + 8]
 	out dx, ax
 	ret
 
 x86_32_out_32:
-	mov edx, [ebp + 8]
-	mov eax, [ebp + 12]
+	mov edx, [esp + 4]
+	mov eax, [esp + 8]
 	out dx, eax
 	ret
 
 x86_32_in_8:
-	mov edx, [ebp + 8]
+	mov edx, [esp + 4]
 	in al, dx
 	ret
 
 x86_32_in_16:
-	mov edx, [ebp + 8]
+	mov edx, [esp + 4]
 	in ax, dx
 	ret
 
 x86_32_in_32:
-	mov edx, [ebp + 8]
+	mov edx, [esp + 4]
 	in eax, dx
 	ret
 
